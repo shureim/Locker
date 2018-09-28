a0476af7bfd447c805ea62d26afbdab868583e4b
tarick ali <tarickaliabdi@gmail.com>
2018-09-28 10:39:58 +0300

    added a init ,method that checks if the object is initialized properly in the test_class.py

--- test_class.py
+++ test_class.py
@@ -16,16 +16,16 @@ class TestUser(unittest.TestCase) :
         set up method  to run before each test cases
         """
 
-#         self.new_user("John", "Facebook", "john@gmail.com" "James001")
+        self.new_user("John", "Facebook", "john@gmail.com" "James001")
 
-#      def test__init__(self) :
-#         """
-#         test init  test case to test if the object is initialized properly
-#         """
-#         self.assertEqual(self.new_user.userName, "John")
-#         self.assertEqual(self.new_user.accountName, "Facebook")
-#         self.assertEqual(self.new_user.email, "john@gmail.com")
-#         self.assertEqual(self.new_user.password, "James001")
+     def test__init__(self) :
+        """
+        test init  test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_user.userName, "John")
+        self.assertEqual(self.new_user.accountName, "Facebook")
+        self.assertEqual(self.new_user.email, "john@gmail.com")
+        self.assertEqual(self.new_user.password, "James001")
 
-# if __name__ == "__main__" :
-#     unittest.main()        
+if __name__ == "__main__" :
+    unittest.main()        
