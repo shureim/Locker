78940b6942011758462a5e69f5f5b07fb36e684a
tarick ali <tarickaliabdi@gmail.com>
2018-09-28 11:10:43 +0300

    added the save user test case to test if the user is saved in test_class.py

--- test_class.py
+++ test_class.py
@@ -16,9 +16,12 @@ class TestUser(unittest.TestCase) :
         set up method  to run before each test cases
         """
 
-        self.new_user("John", "Facebook", "john@gmail.com" "James001")
+        self.new_user = User("John", "Facebook", "john@gmail.com", "James001")
 
-     def test__init__(self) :
+    # def tearDown(self) :
+    #     User.user_list = []
+
+    def test__init__(self) :
         """
         test init  test case to test if the object is initialized properly
         """
@@ -26,6 +29,26 @@ class TestUser(unittest.TestCase) :
         self.assertEqual(self.new_user.accountName, "Facebook")
         self.assertEqual(self.new_user.email, "john@gmail.com")
         self.assertEqual(self.new_user.password, "James001")
+
+    def test_save_user(self) :
+        """
+        test save user test case to test if ths user is saved into the user list.
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+    # def test_save_multiple_user(self) :
+    #     """
+    #     test save multiple user   test case to check if we can save multiple user.
+    #     """
+    #     self.new_user.save_user()
+    #     test.user = User("Tarick", "Instagram", "tarick@gmail.com", "Tarick002")
+    #     test_user.save_user()
+    #     self.assertEqual(len(User.user_list),2)
+
+
+
+
 
 if __name__ == "__main__" :
     unittest.main()        
--- user.py
+++ user.py
@@ -1,24 +1,30 @@
 class User :
+    """
+    class tha generate new instance 
 
- """
- class tha generate new instance 
-
- """ 
+    """ 
+    user_list = [] # empty user list
     def __init__(self, userName, accountName, email, password) :
 
-     """
-     __init__ method that help us create properties of the object
+        """
+        __init__ method that help us create properties of the object
 
-        Args :
-        username :new user username .
-        accountName : new user accountName.
-        email : new user email.
-        password : new user  password.
+            Args :
+            username :new user username .
+            accountName : new user accountName.
+            email : new user email.
+            password : new user  password.
 
-     """
+        """
         self.userName = userName
         self.accountName = accountName
         self.email = email
         self.password = password
+
+    # def save_user(self) :
+    #     """
+    #     save_user method saves user object into users list.
+    #     """
+    #     User.user_list.append(self)
 
 
