2a65803e263955f0e205ffe3b212919c0d28a0f0
tarick ali <tarickaliabdi@gmail.com>
2018-09-28 10:13:30 +0300

    added the init method that helps to define the properties of the object in user.py

--- user.py
+++ user.py
@@ -4,21 +4,21 @@ class User :
  class tha generate new instance 
 
  """ 
-    # def __init__(self, username, accountName, email, password) :
+    def __init__(self, username, accountName, email, password) :
 
-    #  """
-    #  __init__ method that help us create properties of the object
+     """
+     __init__ method that help us create properties of the object
 
-    #     Args :
-    #     username :new user username .
-    #     accountName : new user accountName.
-    #     email : new user email.
-    #     password : new user  password.
+        Args :
+        username :new user username .
+        accountName : new user accountName.
+        email : new user email.
+        password : new user  password.
 
-    #  """
-    #     self.username = username
-    #     self.accountName = accountName
-    #     self.email = email
-    #     self.password = password
+     """
+        self.username = username
+        self.accountName = accountName
+        self.email = email
+        self.password = password
 
 
