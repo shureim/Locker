9a8c75926e921f6eb594fbf8ac89112454771be4
tarick ali <tarickaliabdi@gmail.com>
2018-09-28 10:12:05 +0300

    created class user in the user.py

---
+++ test_class.py
@@ -0,0 +1,12 @@
+# import unittest
+# from user import User
+
+# class TestUser(unittest.TestCase) :
+#     """
+#     Test class that defines test cases for the user behaviour.
+
+#     Args :
+
+#     unittest.TestCase  TestCase class that helps in creating test cases.
+
+#     """
---
+++ user.py
@@ -0,0 +1,24 @@
+class User :
+
+ """
+ class tha generate new instance 
+
+ """ 
+    # def __init__(self, username, accountName, email, password) :
+
+    #  """
+    #  __init__ method that help us create properties of the object
+
+    #     Args :
+    #     username :new user username .
+    #     accountName : new user accountName.
+    #     email : new user email.
+    #     password : new user  password.
+
+    #  """
+    #     self.username = username
+    #     self.accountName = accountName
+    #     self.email = email
+    #     self.password = password
+
+
