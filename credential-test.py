9140e7d5330548254875e1011b28d1420343f4ba
tarick ali <tarickaliabdi@gmail.com>
2018-10-01 08:23:32 +0300

    added test case for saving multiple credential and also deleting credential

--- credential-test.py
+++ credential-test.py
@@ -25,30 +25,30 @@ class TestCredential(unittest.TestCase) :
         self.assertEqual(self.new_credential.accountName, "snapchat")
         self.assertEqual(self.new_credential.email, "chat@gmail.com")
         self.assertEqual(self.new_credential.password, "chat001")
-    # def test_save_credential(self) :
-    #     """
-    #     test save credential test case to test if ths credential is saved into the credential list.
-    #     """
-    #     self.new_credential.save_credential()
-    #     self.assertEqual(len(Credential.credential_list),1)
-    # def test_save_multiple_credential(self) :
-    #     """
-    #     test save multiple credential test case to check if we can save multiple credential.
-    #     """
-    #     self.new_credential.save_credential()
-    #     test_credential = Credential("Instagram", "insta@gmail.com",  "Insta002") #new credential
-    #     test_credential.save_credential()
-    #     self.assertEqual(len(Credential.credential_list),2)
+    def test_save_credential(self) :
+        """
+        test save credential test case to test if ths credential is saved into the credential list.
+        """
+        self.new_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list),1)
+    def test_save_multiple_credential(self) :
+        """
+        test save multiple credential test case to check if we can save multiple credential.
+        """
+        self.new_credential.save_credential()
+        test_credential = Credential("Instagram", "insta@gmail.com",  "Insta002") #new credential
+        test_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list),2)
 
-    # def test_delete_user(self) :
+    def test_delete_user(self) :
 
-    #     """
-    #     test delete user -test case to delete already saved user from the user list
-    #     """
-    #     self.new_credential.save_credential()
-    #     test_credential = Credential("peter", "Peter",  "Peter003") # new user
-    #     test_credential.save_credential()
-    #     self.assertEqual(len(Credential.credential_list),2)
+        """
+        test delete user -test case to delete already saved user from the user list
+        """
+        self.new_credential.save_credential()
+        test_credential = Credential("peter", "Peter",  "Peter003") # new user
+        test_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list),2)
 
 
 if __name__ == "__main__":
